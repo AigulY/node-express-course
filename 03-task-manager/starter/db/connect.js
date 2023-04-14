97449f0cfc6da1b1748bd72c005da105f8225e80
AigulY <azia.kz.a@gmail.com>
2023-04-14 03:01:19 -0400

    week5

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose')
 
-//const connectionString = 
-    //'mongodb+srv://Azia:Qwerty12345@nodeexpressprojects.anm1d2m.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority'
-
 const connectDB = (url) => {
     return  mongoose.connect(url, {
         useNewUrlParser: true,
